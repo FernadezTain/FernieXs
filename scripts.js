b7207d94c7c714ef1b111a70a0f259f9cb819d9d
FernadezTain <fernadezofficial489@gmail.com>
2025-09-27 17:20:42 +0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-<script>
 const DURATIONS = {
     "12h": ["12 ч.", 12 * 3600, 50, 75],
     "24h": ["24 ч.", 24 * 3600, 100, 150],
@@ -50,8 +49,8 @@ function setupPaymentButtons() {
     paymentOptions.innerHTML = `
         <p class="mb-2"><strong>Способ оплаты:</strong></p>
         <div class="flex gap-4 mb-6">
-            <button id="payment-semena" class="payment-button px-4 py-2 rounded-lg hover:scale-105 transition-transform duration-300 bg-gradient-to-r from-indigo-400 to-purple-500 text-white" onclick="selectPayment('Семена', this)">Семена</button>
-            <button id="payment-dc" class="payment-button px-4 py-2 rounded-lg hover:scale-105 transition-transform duration-300 bg-gradient-to-r from-green-400 to-teal-500 text-white" onclick="selectPayment('DigitalCoins', this)">DigitalCoins</button>
+            <button id="payment-semena" class="payment-button px-4 py-2 rounded-lg hover:scale-105 transition-transform duration-300 bg-gradient-to-r from-indigo-400 to-purple-500 text-white">Семена</button>
+            <button id="payment-dc" class="payment-button px-4 py-2 rounded-lg hover:scale-105 transition-transform duration-300 bg-gradient-to-r from-green-400 to-teal-500 text-white">DigitalCoins</button>
         </div>
         <p id="total-price" class="mb-6 font-bold text-lg"></p>
         <button id="pay-button" class="bg-gradient-to-r from-yellow-400 to-orange-500 px-6 py-3 rounded-lg shadow-lg hover:scale-105 transition-transform duration-300">
@@ -59,6 +58,8 @@ function setupPaymentButtons() {
         </button>
     `;
 
+    document.getElementById("payment-semena").onclick = () => selectPayment('Семена', document.getElementById("payment-semena"));
+    document.getElementById("payment-dc").onclick = () => selectPayment('DigitalCoins', document.getElementById("payment-dc"));
     document.getElementById("pay-button").onclick = payInBot;
 }
 
@@ -89,4 +90,3 @@ function payInBot() {
     const url = `https://t.me/FernieUIBot?start=fernieplus_${selectedDuration}_${selectedPayment}`;
     window.open(url, "_blank");
 }
-</script>
